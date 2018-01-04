f2fd22f89f44b854baef28a2b96a3bf4843d63e3
Srdjan Kovacevic <srdjan.kovacevic86@gmail.com>
2018-01-04 23:05:11 +0100

    This commit creates the table with the heigth and width the user chose

--- designs.js
+++ designs.js
@@ -3,8 +3,28 @@
 
 // When size is submitted by the user, call makeGrid()
 
+
+//Call method when Button is clicked
+$('#sizePicker').submit(function(e){
+  e.preventDefault();
+  makeGrid();
+});
+
 function makeGrid() {
+  var gridHeight = $('#input_height').val();
+  var gridWidth = $('#input_width').val();
+  var choosenColor = $('#colorPicker').val();
+  var table = $('#pixel_canvas');
+  createTable(table, gridHeight, gridWidth);
+}
 
-// Your code goes here!
-
+function createTable(table, height, width){
+  table.empty(); // Before creating the table, wipe the old one
+  for (var i = 0; i < height; i++) {
+    var tr = $('<tr></tr>');
+    table.append(tr);
+    for (var j = 0; j < width; j++) {
+      tr.append('<td></td>');
+    }
+  }
 }
